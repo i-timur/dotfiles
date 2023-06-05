77fa053870b05395b6950f5e52b9fcbf4a297601
i-timur <i.timur@mail.ru>
2023-06-05 16:01:35 +0300

    feat: add more remappings

--- nvim/lua/basic.lua
+++ nvim/lua/basic.lua
@@ -6,15 +6,9 @@ set.fileencoding = 'utf-8'
 
 set.termguicolors = true
 
---[[
-vim.api.nvim_create_autocmd('VimEnter', {
-	pattern = '*',
-	command = 'hi Normal guibg=NONE ctermbg=NONE'
-})
---]]
-
 set.number = true
 set.relativenumber = true
+
 set.tabstop = 4
 set.shiftwidth = 4
 set.softtabstop = 4
@@ -22,7 +16,8 @@ set.expandtab = true
 set.smarttab = true
 set.autoindent = true
 set.smartindent = true
-set.scrolloff = 7
+
+set.scrolloff = 8
 
 -- removes delay while switching between normal/insert modes
 set.ttimeout = true
@@ -30,4 +25,7 @@ set.ttimeoutlen = 1
 set.ttyfast = true
 
 set.listchars = {tab = '▷▷⋮', trail = '.', nbsp = '+', eol = '↲'}
+
+set.hlsearch = false
+set.incsearch = true
 
--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,2 +1,23 @@
+local remap = vim.keymap.set
+
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+remap('n', '<leader>pv', vim.cmd.Ex)
+
+remap("n", "J", "mzJ`z")
+remap("n", "<C-d>", "<C-d>zz")
+remap("n", "<C-u>", "<C-u>zz")
+remap("n", "n", "nzzzv")
+remap("n", "N", "Nzzzv")
+
+-- greatest remap ever -> I agree :)
+remap("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+remap({"n", "v"}, "<leader>y", [["+y]])
+remap("n", "<leader>Y", [["+Y]])
+
+remap({"n", "v"}, "<leader>d", [["_d]])
+
+remap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
